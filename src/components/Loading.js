6026e84ba03e7c359451f0c28c621288ecfb7082
Joerez <rezenbiz@gmail.com>
2018-07-28 18:10:58 -0700

    wowzors

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -8,7 +8,7 @@ class Loading extends Component {
         <div className="loading-div">
           <h1 align="center">OddPillars Is Loading</h1>
         </div>
-        <img alt="loading odd pillar" className="loading-img woah rotateComplexOut" src="https://image.ibb.co/mW9pDo/imageedit_11_2098339181.png" />
+        <img alt="loading odd pillar" className="loading-img woah wowzors" src="https://image.ibb.co/mW9pDo/imageedit_11_2098339181.png" />
       </div>
     )
   }
