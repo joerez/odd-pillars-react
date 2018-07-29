798a7e47956712a1ca38350887d9c7b221c0e811
Joerez <rezenbiz@gmail.com>
2018-07-29 11:09:58 -0700

    featured post link fix

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 class FeaturedPosts extends Component {
   render() {
@@ -8,23 +7,23 @@ class FeaturedPosts extends Component {
         <div className="blogPostSection">
           <h1 className="blogSectionTitle">Featured Posts</h1>
 
-          <Link className="containerLink" key="1" to="/post/what-is-western-esotericism">
+          <a className="containerLink" key="1" href="/post/what-is-western-esotericism">
             <div className="blog-post-container waves-effect waves-light">
               <img alt="More and more people are calling themselves Western Esotericists. But what is Western Esotericism?" src="https://cdn.buttercms.com/0q0GlkNTJWVw9xXerN5x" />
               <p key="0" className="post-title">What is Western Esotericism?</p>
               <p key="1" className="post-desc">More and more people are calling themselves Western Esotericists. But what is Western Esotericism?</p>
               <div className="readMoreLink" key="2"><div className="read-more">Read More</div><i className="material-icons allofit">arrow_forward</i></div>
             </div>
-          </Link>
+          </a>
 
-          <Link className="containerLink" key="2" to="/post/the-problem-with-open-occult-communities">
+          <a className="containerLink" key="2" href="/post/the-problem-with-open-occult-communities">
             <div className="blog-post-container waves-effect waves-light">
               <img alt="Many Occultists are turned off by open online Occult Communities for several reasons. Why is there so much fluff?" src="https://cdn.buttercms.com/KPcYB4d2QGKsjJfImcma" />
               <p key="0" className="post-title">The Problem With Open Occult Communities</p>
               <p key="1" className="post-desc">Many Occultists are turned off by open online Occult Communities for several reasons. Why is there so much fluff?</p>
               <div className="readMoreLink" key="2"><div className="read-more">Read More</div><i className="material-icons allofit">arrow_forward</i></div>
             </div>
-          </Link>
+          </a>
 
 
 
